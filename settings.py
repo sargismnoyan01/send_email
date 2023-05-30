d7b8aa7757cb801ccbcb7139ebb588eb4152efc5
sargismnoyan01 <128826963+sargismnoyan01@users.noreply.github.com>
2023-05-30 19:56:23 +0400

    for send   settings.py

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='mnoyansargis@gmail.com'
-EMAIL_HOST_PASSWORD='hjngktmywjirrvjn'
+EMAIL_HOST_PASSWORD='your password'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
